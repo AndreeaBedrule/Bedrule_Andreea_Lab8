4b5c06fa62c533f14b3ee50762c5c4ebea07d427
andreea andra <andrea_bedr@yahoo.com>
2021-11-27 21:07:34 +0200

    Add project files.

---
+++ Data/Bedrule_Andreea_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Bedrule_Andreea_Lab8.Models;
+
+namespace Bedrule_Andreea_Lab8.Data
+{
+    public class Bedrule_Andreea_Lab8Context : DbContext
+    {
+        public Bedrule_Andreea_Lab8Context (DbContextOptions<Bedrule_Andreea_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bedrule_Andreea_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Bedrule_Andreea_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
